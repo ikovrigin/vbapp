46426e1e936c8073f7c450782f63b291560690bb
Igor Kovrigin <ikovrigin@kmilearning.com>
2023-10-06 22:57:06 +0300

    inmemory sqlite

--- backend/models/database.py
+++ backend/models/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
+SQLALCHEMY_DATABASE_URL = "sqlite://"
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:vbapp!password@vbapp.c9ugha32qj54.eu-north-1.rds.amazonaws.com/vbapp"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL,
+                       connect_args={'check_same_thread':False},
+                       poolclass=StaticPool)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
