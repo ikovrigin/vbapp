b6f9e0428ec20c1053960b70cb3f2ff8dceb6c58
Igor Kovrigin <ikovrigin@kmilearning.com>
2023-10-06 22:10:34 +0300

    rds

--- backend/models/database.py
+++ backend/models/database.py
@@ -1,10 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:vbapp!password@vbapp.c9ugha32qj54.eu-north-1.rds.amazonaws.com/vbapp"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
