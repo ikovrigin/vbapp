717c619632c3b5d9edce6f061ebd3a85a21104b2
Igor Kovrigin <ikovrigin@kmilearning.com>
2023-10-06 21:58:36 +0300

    prepare for lambda

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+import mangum
 from fastapi import FastAPI
 from models import Base
 from models.database import engine
@@ -6,7 +7,8 @@ from routes import users, items, token
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-app.include_router(token.router, prefix='/api')
-app.include_router(users.router, prefix='/api')
-app.include_router(items.router, prefix='/api')
+handler = mangum.Mangum(app, api_gateway_base_path='/api')
 
+app.include_router(token.router)
+app.include_router(users.router)
+app.include_router(items.router)
