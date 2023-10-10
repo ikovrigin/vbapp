2eac5ca8d2cbd4c2c097ffd4c94fa2bf601d7565
Igor Kovrigin <ikovrigin@kmilearning.com>
2023-10-10 22:49:20 +0300

    remove / at the end of urls

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -38,11 +38,9 @@ def read_user_me(current_user: Annotated[User, Depends(get_current_user)]):
     return current_user
 
 
-@router.get("/users/{user_id}/", response_model=schemas.User)
+@router.get("/users/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
-
-
