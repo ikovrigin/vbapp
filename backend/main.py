6396128e5d7ec13aa09a6ad848ca578a004b795e
Igor Kovrigin <ikovrigin@kmilearning.com>
2023-10-11 23:13:06 +0300

    CORS

--- backend/main.py
+++ backend/main.py
@@ -17,8 +17,6 @@ app.add_middleware(
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
-
 app.include_router(token.router, prefix='/api')
 app.include_router(users.router, prefix='/api')
 app.include_router(items.router, prefix='/api')
