0fd7ae17bee3a860ee623f3cedc76e54e2198270
Igor Kovrigin <ikovrigin@kmilearning.com>
2023-10-11 23:22:54 +0300

    CORS

--- backend/main.py
+++ backend/main.py
@@ -7,14 +7,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 Base.metadata.create_all(bind=engine)
 
 app.include_router(token.router, prefix='/api')
