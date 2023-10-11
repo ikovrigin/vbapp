15923440f1348bad67fb8e43f6da25c6153a06a1
Igor Kovrigin <ikovrigin@kmilearning.com>
2023-10-11 23:00:47 +0300

    CORS

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,17 @@ from fastapi import FastAPI
 from models import Base
 from models.database import engine
 from routes import users, items, token
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
